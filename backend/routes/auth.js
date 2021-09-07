c33a340519756ae49a20c0e80919ecc2f0030eb1
Manish Bhamare <mbb7224@gmail.com>
2021-09-07 15:18:29 +0530

    Added password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,25 +2,53 @@
 const express = require('express')
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 const router = express.Router()
 
-router.post('/', 
+const JWT_SECRETE = "manishisagoodb$oy"
+
+router.post('/createuser', 
         body('name' , 'Enter a valid name!').isLength({ min: 3 }),
         body('email' ,'Enter a valid email!').isEmail(),
         body('password' , 'Password must be 5 characters at least!').isLength({ min: 5 }),
-        (req,res)=>{
+        async (req,res)=>{
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-    
-        User.create({
+        
+        try{
+
+        let user = await User.findOne({email : req.body.email});
+        if(user){
+            return res.status(400).json({error : "Sorry user with this email already exists!"});
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        
+        secPass = await bcrypt.hash(req.body.password, salt)
+
+        user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-          }).then(user => res.json(user))
-          .catch(err=> {console.log(err)
-          res.json({error : 'Please enter a unique value for email!', message : err.message})})
+            password: secPass,
+          })
+
+        const data = {
+            user : {
+                id : user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRETE)
+        
+        res.json({authToken});
+        }
+        catch(error){
+            console.error(error.message)
+            res.status(500).send("Some error occured!")
+        }
 })
 
 module.exports = router
