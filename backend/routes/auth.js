54fe8a80be8119bf435e8dcea90d2c8d6365db71
Manish Bhamare <mbb7224@gmail.com>
2021-09-07 16:45:37 +0530

    Created login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,8 +47,50 @@ router.post('/createuser',
         }
         catch(error){
             console.error(error.message)
-            res.status(500).send("Some error occured!")
+            res.status(500).send("Internal server error!")
         }
 })
+
+
+router.post('/login', 
+        body('email' ,'Enter a valid email!').isEmail(),
+        body('password' , 'Password cannot be blank').exists(),
+        async (req,res)=>{
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {email, password} = req.body;
+
+        try{
+            let user = await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error : "Please try to login with correct credentials!"});
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if(!passwordCompare){
+                return res.status(400).json({error : "Please try to login with correct credentials!"});
+            }
+
+            const data = {
+                user : {
+                    id : user.id
+                }
+            }
+    
+            const authToken = jwt.sign(data, JWT_SECRETE);
+            res.json({authToken});
+            
+        }
+        catch(error){
+            console.error(error.message)
+            res.status(500).send("Internal server error!")
+        }
+
+    })
+
 
 module.exports = router
