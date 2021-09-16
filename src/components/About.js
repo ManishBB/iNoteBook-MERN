6b3a3d11eaac6567d709a82b7a9522ba855cf0be
Manish Bhamare <mbb7224@gmail.com>
2021-09-16 22:48:54 +0530

    Used useLocation Hook

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,9 @@ import React , { useContext, useEffect } from 'react';
 import noteContext from '../context/notes/noteContext';
 
 const About = () => {
-
-    const a = useContext(noteContext);
-
-    useEffect(() => {
-        a.update()
-    }, [])
-
     return (
         <div>
-            This is About {a.state.name} & he is in {a.state.class}
+            This is About 
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name"  : "Manish",
-        "class" : "15b"
-    }
-
-    const [state, setstate] = useState(s1);
-
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name" : "Manya",
-                "class" : "7c"
-            })
-        }, 1000);
-    }
 
     return(
-        <NoteContext.Provider value={{state , update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
